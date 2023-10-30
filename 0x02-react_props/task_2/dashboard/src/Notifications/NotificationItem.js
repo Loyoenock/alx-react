15bc92c5c309183eef746de59f8eb496d964e41a
Loyoenock <loyoenock@gmail.com>
2023-10-30 22:09:20 +0000

    4. Props types & Default props & Shapes and 5. Shapes, Loops, and Keys

--- 0x02-react_props/task_2/dashboard/src/Notifications/NotificationItem.js
+++ 0x02-react_props/task_2/dashboard/src/Notifications/NotificationItem.js
@@ -1,15 +1,15 @@
-import React from "react";
-import "./Notifications.css";
+import React from 'react';
+import './Notifications.css';
 
 const NotificationItem = ({ type, html, value }) => {
-  return (
-    <>
-      {type && value ? <li data-notification-type={type}>{value}</li> : null}
-      {html ? (
-        <li data-urgent dangerouslySetInnerHTML={{ __html: html }}></li>
-      ) : null}
-    </>
-  );
+	return (
+		<>
+			{type && value ? <li data-notification-type={type}>{value}</li> : null}
+			{html ? (
+				<li data-urgent dangerouslySetInnerHTML={{ __html: html }}></li>
+			) : null}
+		</>
+	);
 };
 
 export default NotificationItem;
